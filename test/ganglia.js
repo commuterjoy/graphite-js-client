056a654112d51822d44ff5d1896ec3996e59658f
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-03-20 13:40:53 +0000

    A model for ganglia target

--- test/ganglia.js
+++ test/ganglia.js
@@ -17,7 +17,22 @@ describe('Ganglia client', function(){
         g.toUrl().should.be.equal('http://graphite.guprod.gnl/render?target=bar.sum&target=foo-%C3%A5-sum&from=-1hours&format=json&jsonp=?')
     })
     
-    xit('should push ganglia targets with aliases', function() {}) 
+    it('should push ganglia targets with functions', function() {
+
+        var t = new ganglia.target('ganglia.GU-PROD-Frontend')
+                     .exclude('__SummaryInfo__')
+                     .averageSeries()
+                     .alias('foo')
+
+        t.toQueryString().should.be.equal('alias(averageSeries(exclude(ganglia.GU-PROD-Frontend,"__SummaryInfo__")),"foo")')
+        
+        var t = new ganglia.target('ganglia.GU-PROD-Frontend')
+                     .timeshift('7d')
+
+        t.toQueryString().should.be.equal('timeshift(ganglia.GU-PROD-Frontend,"7d")')
+
+    })
+
     xit('should be able to specify the output format', function() {})
     xit('should be able to specify the time series duration', function() {})
     xit('should be able to specify the jsonp callback', function() {})
